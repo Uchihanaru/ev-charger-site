d60881415f6a345438940cead6e297cd2d6cb469
Uchihanaru <jrrobert9064@gmail.com>
2026-01-03 16:59:53 +0530

    Force deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* This allows the build to succeed even if there are type errors */
   typescript: {
     ignoreBuildErrors: true,
   },
-  /* This ignores linting errors */
   eslint: {
     ignoreDuringBuilds: true,
   },
